620066db74e0fe2ef7c78d6374985eea8bfab078
Aditya Dhage <33369663+adi-sd@users.noreply.github.com>
2024-07-30 21:51:47 -0700

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div>
             <h1 className="text-blue-600 text-2xl font-medium">Hi there from task-it!</h1>
-            <Link href={"/home"}>Home Page</Link>
+            <Link href={"/home"}>Home</Link>
         </div>
     );
 }
