c0d0ab5224cd84d9592b59e2f58afdfce942fab4
Aditya Dhage <adhage93@gmail.com>
2024-09-12 11:14:03 -0700

    Added responsive homepage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,20 +7,22 @@ const RootPage = async () => {
         <main className="w-full h-full overflow-hidden">
             <NavBar className="w-full h-[60px]"></NavBar>
             <div className="w-full h-[calc(100vh-60px)] flex flex-col gap-y-6 items-center justify-center">
-                <div className="w-[700px] h-[300px] flex flex-col gap-y-10 items-center justify-center border-4 border-slate-500 rounded-xl">
-                    <div>
-                        <h1 className="text-2xl font-semibold">
-                            Welcome to <span className="font-mono font-extrabold text-indigo-700">task-it</span> Please
-                            Login/Register to proceed!
-                        </h1>
-                    </div>
-                    <div className="flex gap-x-4">
-                        <div>
-                            <AuthDialog>
-                                <Button variant="default" size="lg" className="text-lg font-semibold">
-                                    Login / Register
-                                </Button>
-                            </AuthDialog>
+                <div className="sm:w-[700px] m-10 h-[300px] border-4 border-slate-500 rounded-xl">
+                    <div className="w-full h-full flex flex-col gap-y-10 p-10 items-center justify-center">
+                        <div className="sm:text-wrap text-center">
+                            <h1 className="text-2xl font-semibold">
+                                Welcome to <span className="font-mono font-extrabold text-indigo-700">task-it</span>{" "}
+                                Please Login/Register to proceed!
+                            </h1>
+                        </div>
+                        <div className="flex gap-x-4">
+                            <div>
+                                <AuthDialog>
+                                    <Button variant="default" size="lg" className="text-lg font-semibold">
+                                        Login / Register
+                                    </Button>
+                                </AuthDialog>
+                            </div>
                         </div>
                     </div>
                 </div>
